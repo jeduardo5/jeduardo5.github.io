467329da456c9bec9242c6e7e74c1126ee22213a
James E. Jackson <jms.jcksn5@gmail.com>
2018-06-25 15:51:52 -0700

    Redesigned Website Portfolio. Added Dimension HTML5 Theme.

---
+++ jeduardo5.github.io/src/components/Main.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import pic01 from '../images/pic01.jpg'
+import pic02 from '../images/pic02.jpg'
+import pic03 from '../images/pic03.jpg'
+
+class Main extends React.Component {
+  render() {
+
+    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+
+    return (
+      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Intro</h2>
+          <span className="image main"><img src={pic01} alt="" /></span>
+          <p></p>{close}
+        </article>
+
+        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Work</h2>
+          <span className="image main"><img src={pic01} alt="" /></span>
+          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          {close}
+        </article>
+
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">About</h2>
+          <span className="image main"><img src={pic02} alt="" /></span>
+         <p>I have experience working on Software Engineering teams, doing pair programming, using VCS and git to work on large company codebases, making pull requests on a team with over 12 engineers, doing and responding to code reviews, using Third-Party APIs, and being flexible and motivated enough to learn emerging languages/libraries on the fly. 
+          On the experience side I may be sometimes barely meeting requirements but I have worked with over 5 Fortune 500 companies during my college career doing internships each year. I did not get a full-time role after my final summer internship with SurveyMonkey due to budget cuts (and I can provide references to prove that)...
+          But I abide by an entrepreneurial self-starting spirit so I have taken on a contract role from a real company that has given me relevant experience creating, testing and publishing an android app on the Google Play Store. This has helped me realize my passion for mobile development and has given me plenty of fast-paced exposure. I think that this by far means that I would be an asset to your team if given the chance.</p>
+          {close}
+        </article>
+
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Contact</h2>
+          <form method="post" action="#">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="reset" value="Reset" /></li>
+            </ul>
+          </form>
+          <ul className="icons">
+            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+          </ul>
+          {close}
+        </article>
+
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: React.PropTypes.object,
+  article: React.PropTypes.string,
+  articleTimeout: React.PropTypes.bool,
+  onCloseArticle: React.PropTypes.func,
+  timeout: React.PropTypes.bool
+}
+
+export default Main
